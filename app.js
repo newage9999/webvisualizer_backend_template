afc0d70d0c08f7c1f0b36300ef5b1be6a053c6a8
MCarmen <detoro.mcarmen@gmail.com>
2023-03-23 09:50:26 +0100

    Formatting the fetch into a one level promise chain. Added the code to parse an anchor link with cheerio

--- app.js
+++ app.js
@@ -4,41 +4,47 @@ const archiver = require('archiver');
 
 const startJSON = (req, res) => {
     // We fetch the webpage and convert the HTML code to text
-    fetch(...).then(response => {
-        response.text().then(text => {
-            // We parse the HTML text using cheerio
-            const $ = cheerio.load(text);
-            // We iterate each <a/> tag
-            $('a').each(function() {
-                // We process the <a/> tag
-                ...
-            });
+    fetch(...)
+    //get the promise value the fetch resolves to (response) and 
+    //call its text method:(response.text()). See the fetch API
+    //Now that we have the HTML in (text) we parse it:
+    .then(text => {
+        // We parse the HTML text using cheerio
+        const $ = cheerio.load(text);
+        // We iterate each <a/> tag
+        $('a').each((i,link)=>{
+            // We get the <a/> tag
+            let href = $(this).attr('href');
+            // We process the <a/> tag
+            //    ...
+            })
         });
-    });
-};
+    }
 
 const startZIP = (req, res) => {
     // We fetch the webpage and convert the HTML code to text
-    fetch(...).then(response => {
-        response.text().then(text => {
+    fetch(...)
+    //get the promise value the fetch resolves to (response) and 
+    //call its text method:(response.text()). See the fetch API
+    //Now that we have the HTML in (text) we parse it:
+    .then(text => {
             // We parse the HTML text using cheerio
             const $ = cheerio.load(text);
             // We iterate each <a/> tag
             $('a').each(function() {
                 // We process the <a/> tag
-                ...
+                //...
                 fetch(...)
                 .then(result => result.arrayBuffer())
                 .then(arrayBuffer => {
                     // We convert the array buffer to a Buffer object
                     const buffer = Buffer.from(arrayBuffer);
                     // We put append the buffer into the zip
-                    zip.append(Buffer.from(buffer), { name: 'a' });
+                    // Here goes appending the StringBuffer: Buffer.from(buffer) in the zip.                    
                 });
             });
         });
-    });
-};
+    }        
 
 const app = express();
 const port = 3000;
